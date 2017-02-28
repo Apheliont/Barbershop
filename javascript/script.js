61cb8db0a56a4be5c6702673f41b5568465f5878
Apheliont <apheliont@gmail.com>
2017-02-28 22:35:16 +0300

    mobile final v2

--- javascript/script.js
+++ javascript/script.js
@@ -13,6 +13,13 @@ var closeLoginFormBtn = document.querySelector(".login-form__close");
 var loginForm = document.querySelector(".login-form");
 var loginFormInputLogin = document.querySelector(".login-form__login");
 var loginFormInputPassword = document.querySelector(".login-form__password");
+var submitFormBtn = document.querySelector(".login-form__submit");
+var socialDescription = document.querySelectorAll(".social-list__link-description");
+
+//Скрыть описание социальных ссылок. Вместо них иконки
+for(var i = 0; i < socialDescription.length; i++) {
+    socialDescription[i].classList.add("social-list__link-description--hidden");
+}
 
 function closeMenu () {
     for(var i = 0; i < menu.length; i++) {
@@ -23,10 +30,30 @@ function closeMenu () {
     visibleMenuItem.classList.add("main-nav__menu-item--visible");
 }
 
+// Кнопка закрытия формы логина
 closeLoginFormBtn.addEventListener('click', function(){
-    loginForm.classList.remove("login-form--visible");
+    loginForm.classList.remove("login-form--formerror");
+    loginForm.classList.add("login-form--close");
+    setTimeout(function(){
+        loginForm.classList.remove("login-form--close");
+        loginForm.classList.remove("login-form--visible");
+    }, 500);
+
 });
 
+// Кнопка отправки формы логина
+submitFormBtn.addEventListener('click', function() {
+    if(!(loginFormInputLogin.value && loginFormInputPassword.value)) {
+        event.preventDefault();
+        loginForm.classList.remove("login-form--formerror");
+        void loginForm.offsetWidth;
+        loginForm.classList.add("login-form--formerror");
+    } else {
+        localStorage.setItem("login", loginFormInputLogin.value);
+    }
+});
+
+// Обработчик события на кнопке login в главном меню
 loginButton.addEventListener('click', function(){
     event.preventDefault();
     loginForm.classList.add("login-form--visible");
@@ -39,7 +66,7 @@ loginButton.addEventListener('click', function(){
     }
     closeMenu ();
 });
-
+// Обработчик события на гамбургере
 menuButton.addEventListener('click', function() {
     for(var i = 0; i < menu.length; i++) {
         if (!menu[i].classList.contains("main-nav__menu-item--active")) {
